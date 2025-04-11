17bcefec4738c771f180db96d2a7567871c3f46f
Scott Fleming <sdflem@gmail.com>
2025-04-11 15:17:21 -0500

    Add activity 1 tests

--- test/controllers/user_limericks_controller_test.rb
+++ test/controllers/user_limericks_controller_test.rb
@@ -6,4 +6,15 @@ class UserLimericksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  include Devise::Test::IntegrationHelpers
+
+  test 'should get index' do
+    user = users(:one)
+    limericks_size = user.limericks.size
+    sign_in user
+    get user_limericks_url(user)
+    assert_response :success
+    assert_select 'div.card', limericks_size
+  end
 end

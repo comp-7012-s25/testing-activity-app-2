a3af63769735a2e97fbcb6d5ebfe9f4e6b9745b6
Scott Fleming <sdflem@gmail.com>
2025-04-11 15:17:13 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,54 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Users
+
+alice = User.create!(
+  email:    'alice@email.com',
+  password: 'password'
+)
+
+bob = User.create!(
+  email:    'bob@email.com',
+  password: 'password'
+)
+
+# Alice's Limericks
+
+Limerick.create!(
+  title:         'There was an old man of Nantucket',
+  limerick_text: "There was an old man of Nantucket\nWho kept all his cash in a bucket;\nBut his daughter, named Nan\nRan away with a man—\nAnd as far as the bucket, Nantucket.",
+  author:        alice
+)
+
+Limerick.create!(
+  title:         'A certain young fellow named Bee-Bee',
+  limerick_text: "A certain young fellow named Bee-Bee\nWished to wed a woman named Phoebe.\n\"But,\" he said, \"I must see\nWhat the clerical fee\nBe before Phoebe be Phoebe Bee-Bee.\"",
+  author:        alice
+)
+
+Limerick.create!(
+  title:         'There once was a fly on the wall',
+  limerick_text: "There once was a fly on the wall\nI wonder why didn't it fall\nBecause its feet stuck\nOr was it just luck\nOr does gravity miss things so small?",
+  author:        alice
+)
+
+# Bob's Limericks
+
+Limerick.create!(
+  title:         'There once was a farmer from Leeds',
+  limerick_text: "There once was a farmer from Leeds,\nWho swallowed a packet of seeds.\nIt soon came to pass,\nHe was covered with grass,\nBut has all the tomatoes he needs.",
+  author:        bob
+)
+
+Limerick.create!(
+  title:         'A canner, exceedingly canny',
+  limerick_text: "A canner, exceedingly canny\nOne morning remarked to his granny,\n\"A canner can can\nAnything that he can;\nBut a canner can't can a can, can he?\"",
+  author:        bob
+)
+
+Limerick.create!(
+  title:         'There was a young man from Dealing',
+  limerick_text: "There was a young man from Dealing\nWho caught the bus for Ealing.\nIt said on the door\n\"Don't spit on the floor\"\nSo he jumped up and spat on the ceiling",
+  author:        bob
+)
